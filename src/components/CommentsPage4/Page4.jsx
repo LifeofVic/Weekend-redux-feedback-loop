a1d7ceecadb424b3a4a31c454c2081ac4a4581d6
Victor Llapa <victorllapa@gmail.com>
2022-12-03 14:18:50 -0600

    Added the basis for page 4 - comments sections

--- src/components/CommentsPage4/Page4.jsx
+++ src/components/CommentsPage4/Page4.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 
-export default function SupportPage() {
-	/**This will return the input field that will receive the integer for the question: //? How well are you being supported? 
+export default function CommentsPage() {
+	/**This will return the input field that will receive the string for the question:  //? Any Comments you want to leave? 
 
 That data will then be added to the state that was created in the index.js file.   
 
@@ -11,7 +11,7 @@ const Feedback = {
 id: //* created.
 feeling: //*number provided by [FEELINGS PAGE]
 understanding: //*number provided by [UNDERSTANDING PAGE]
-support: //! INSERTED BY THIS FILE. 
-comments: //NEXT PAGE
+support:  //*number provided by [SUPPORT PAGE]
+comments: //! INSERTED BY THIS FILE.
 */
 }
