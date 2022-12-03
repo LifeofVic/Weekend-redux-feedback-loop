0840f1f8d2e31d20f1883ca791a7ceac7c6b28c1
Victor Llapa <victorllapa@gmail.com>
2022-12-03 14:11:29 -0600

    Added some info on page 2.

--- src/components/FeelingsPage1/Page1.jsx
+++ src/components/FeelingsPage1/Page1.jsx
@@ -2,10 +2,17 @@ import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 
-export default function Feelings() {
+export default function FeelingsPage() {
 	/**This will return the input field that will receive the integer for the question:  How are you feeling today? 
 
 That data will then be added to the state that was created in the index.js file. 
+const Feedback = {
+id: //* created.
+feeling://! INSERTED BY THIS FILE. 
+understanding: 
+support: 
+comments: 
+}
 
  */
 }
---
+++ src/components/UnderstandingPage2/Page2.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+
+export default function UnderstandingPage() {
+	/**This will return the input field that will receive the integer for the question:  How well are you understanding the content? 
+
+That data will then be added to the state that was created in the index.js file.   
+
+const Feedback = {
+id: //* created.
+feeling: //*number provided. 
+understanding: //! INSERTED BY THIS FILE. 
+support: 
+comments: 
+}
+
+ */
+}
