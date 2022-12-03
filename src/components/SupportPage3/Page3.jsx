0cc1c4491f15c28d0eb0f39f22a48adc79a96d5f
Victor Llapa <victorllapa@gmail.com>
2022-12-03 14:14:55 -0600

    Created the basis for the Supoort page

--- src/components/SupportPage3/Page3.jsx
+++ src/components/SupportPage3/Page3.jsx
@@ -2,18 +2,16 @@ import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 
-export default function UnderstandingPage() {
+export default function SupportPage() {
 	/**This will return the input field that will receive the integer for the question:  How well are you understanding the content? 
 
 That data will then be added to the state that was created in the index.js file.   
 
 const Feedback = {
 id: //* created.
-feeling: //*number provided by [FEELINGS PAGE] 
-understanding: //! INSERTED BY THIS FILE. 
-support: 
+feeling: //*number provided by [FEELINGS PAGE]
+understanding: //*number provided by [UNDERSTANDING PAGE]
+support: //! INSERTED BY THIS FILE. 
 comments: 
-}
-
- */
+*/
 }
