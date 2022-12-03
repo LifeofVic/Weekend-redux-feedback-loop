b1719a12f45ced586d9a7c6639700509ac9db4a8
Victor Llapa <victorllapa@gmail.com>
2022-12-03 15:04:49 -0600

    added the reducers to index.js that will be used where needed in the pages

--- src/index.js
+++ src/index.js
@@ -3,7 +3,49 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+	combineReducers({ userFeedback }),
+	applyMiddleware(logger)
+);
+
+const userFeedback =
+	((state = {
+		feeling: 0,
+		understanding: 0,
+		support: 0,
+		comments: '',
+	}),
+	(action) => {
+		if (action.type === 'SET_FEELING') {
+			return { state, feeling: action.payload };
+		}
+		if (action.type === 'SET_UNDERSTANDING') {
+			return { state, understanding: action.payload };
+		}
+		if (action.type === 'SET_SUPPORT') {
+			return { state, support: action.payload };
+		}
+		if (action.type === 'SET_COMMENTS') {
+			return { state, comments: action.payload };
+		}
+		if (action.type === 'CLEAR_FEEDBACK') {
+			return {
+				feeling: 0,
+				understanding: 0,
+				support: 0,
+				comments: '',
+			};
+		}
+	});
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
 registerServiceWorker();
-
